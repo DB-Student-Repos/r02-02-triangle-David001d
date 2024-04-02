42b7298b431f4263445082aa640202d9e3f089ae
David O <148862921+David001d@users.noreply.github.com>
2024-04-03 01:52:27 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,29 @@
-pub struct Triangle;
+ pub struct Triangle {
+    sides: [u64; 3],
+}
 
 impl Triangle {
     pub fn build(sides: [u64; 3]) -> Option<Triangle> {
-        unimplemented!("Construct new Triangle from following sides: {sides:?}. Return None if the sides are invalid.");
+        let [a, b, c] = sides;
+        if a + b > c && a + c > b && b + c > a && a > 0 && b > 0 && c > 0 {
+            Some(Triangle { sides })
+        } else {
+            None
+        }
     }
 
     pub fn is_equilateral(&self) -> bool {
-        unimplemented!("Determine if the Triangle is equilateral.");
+        let [a, b, c] = self.sides;
+        a == b && b == c
     }
 
     pub fn is_scalene(&self) -> bool {
-        unimplemented!("Determine if the Triangle is scalene.");
+        let [a, b, c] = self.sides;
+        a != b && b != c && a != c
     }
 
     pub fn is_isosceles(&self) -> bool {
-        unimplemented!("Determine if the Triangle is isosceles.");
+        let [a, b, c] = self.sides;
+        a == b || b == c || a == c
     }
 }
